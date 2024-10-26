edb9b851c12b222446b34539ccade88c5bbcb42b
Lenaxia <github@thetall.gent>
2024-10-25 17:44:59 -0700

    Got slackagent working to do basic responses, now need to do message packaging

--- agents/slack_agent/agent.py
+++ agents/slack_agent/agent.py
@@ -9,9 +9,6 @@ from shared_tools.message_bus import MessageBus, MessageType
 from pydantic import BaseModel
 import logging
 
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 class SlackAgentConfig(BaseModel):
     slack_channel: str
@@ -22,26 +19,45 @@ class SlackAgentConfig(BaseModel):
 class SlackAgent(BaseAgent):
     slack_app_token = os.environ.get("SLACK_APP_TOKEN")
     slack_bot_token = os.environ.get("SLACK_BOT_TOKEN")
-    bolt_app = App(token=slack_bot_token)
-
-    @bolt_app.middleware
-    def log_request(logger, body, next):
-        logger.debug(body)
-        return next()
+    app = App(token=slack_bot_token)
 
     def __init__(self, logger, llm_factory: LLMFactory, message_bus: MessageBus, agent_id: str, config: Dict = None):
+        self.logger = logger
         super().__init__(logger, llm_factory, message_bus, agent_id, config=config)
         self.config = SlackAgentConfig(**config)
-
+        self.setup_middleware()
+        self.setup_events()
+        self.setup_error_handler()
         self.subscribe_to_messages(MessageType.SEND_MESSAGE, self.handle_send_message)
-
         self.initialize()
+
+    def setup_middleware(self):
+        @self.app.middleware
+        def log_request(logger, body, next):
+            logger.debug(body)
+            return next()
+
+    def setup_events(self):
+        @self.app.event("app_mention")
+        def handle_app_mention(event, say):
+            metadata = self.event_metadata(event)
+            self.publish_message(MessageType.INCOMING_REQUEST, {"event": event, "metadata": metadata})
+            say(f"Event: {event}\n\nMetadata: {metadata}")
+
+        @self.app.event("message")
+        def handle_message(event, say, logger):
+            pass
+
+    def setup_error_handler(self):
+        @self.app.error
+        def global_error_handler(error, body, logger):
+            logger.exception(error)
+            logger.info(body)
 
     def initialize(self):
         # Start the SocketModeHandler in a separate thread
         socket_mode_thread = threading.Thread(target=self.start_socket_mode_handler)
         socket_mode_thread.start()
-        #self.start_socket_mode_handler()
 
         # Post online message to the preset channel
         self.post_message_to_channel(self.config.status_channel, self.config.online_message)
@@ -51,8 +67,8 @@ class SlackAgent(BaseAgent):
             self.logger.error("Slack app token or bot token not provided")
             return
 
-        handler = SocketModeHandler(self.bolt_app, self.slack_app_token)
-        logger.info("Starting Socket Mode Handler")
+        handler = SocketModeHandler(self.app, self.slack_app_token)
+        self.logger.info("Starting Socket Mode Handler")
         handler.start()
 
         # Add additional logging for socket events
@@ -61,7 +77,7 @@ class SlackAgent(BaseAgent):
 
     def post_message_to_channel(self, channel, message):
         try:
-            self.bolt_app.client.chat_postMessage(channel=channel, text=message)
+            self.app.client.chat_postMessage(channel=channel, text=message)
         except Exception as e:
             self.logger.error(f"Failed to post message to Slack channel: {e}")
 
@@ -74,30 +90,12 @@ class SlackAgent(BaseAgent):
             "timestamp": event.get("ts", None)
         }
 
-    @bolt_app.event("message")
-    def handle_event(self, event, say, logger):
-        print("argh")
-        metadata = self.event_metadata(event)
-        self.publish_message(MessageType.INCOMING_REQUEST, {"event": event, "metadata": metadata})
-        logger.info(metadata)
-        print("yar")
-        say("What's up?")
-
-    @bolt_app.event("app_mention")
-    def handle_app_mention(self, event, say):
-        say("What's up?")
-
     def handle_send_message(self, message):
         event = message["event"]
         metadata = message["metadata"]
         channel = metadata.get("channel", None)
         if channel:
-            self.bolt_app.client.chat_postMessage(channel=channel, text=event["text"])
-
-    @bolt_app.error
-    def global_error_handler(self, error, body, logger):
-        logger.exception(error)
-        logger.info(body)
+            self.app.client.chat_postMessage(channel=channel, text=event["text"])
 
     def setup(self):
         pass
