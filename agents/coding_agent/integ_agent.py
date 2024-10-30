607e91e0e9d3b0a57dbed37cc6b28fdf1eee6cbd
Lenaxia <github@thetall.gent>
2024-10-29 22:02:01 -0700

    incomplete coding agent

---
+++ agents/coding_agent/integ_agent.py
@@ -0,0 +1,109 @@
+import unittest
+from unittest.mock import Mock, patch
+from agents.coding_agent.agent import CodingAgent
+from agents.coding_agent.tools.core import draft_code, retrieve_context, use_aider
+from agents.coding_agent.types import CodingState
+from common.task_graph_v2 import TaskPlanOutput
+from llm_provider.factory import LLMFactory, LLMType
+from common.message_bus import MessageBus
+from logging import Logger
+from langchain_core.messages.ai import AIMessage
+from langchain_aws import ChatBedrock
+from config.bedrock_config import BedrockConfig
+import operator
+
+class TestSolverAgentIntegration(unittest.TestCase):
+    def setUp(self):
+        self.logger = Logger(__name__)
+
+        # Configure the LLMFactory
+        self.llm_factory = LLMFactory({})
+        bedrock_config = BedrockConfig(
+            name="bedrock", model='anthropic.claude-3-sonnet-20240229-v1:0'
+        )
+        self.llm_factory.add_config(LLMType.DEFAULT, bedrock_config)
+        self.message_bus = Mock(MessageBus)
+        self.agent_id = "solver_agent"
+        self.config = {}
+        self.agent = CodingAgent(self.logger, self.llm_factory, self.message_bus, self.agent_id, self.config)
+        
+
+                
+    def test_retrieve_context(self):
+        task_data = {
+            "prompt": "Update all references to metrics manager update_metrics to include the new timestamp parameter.",
+        }
+
+        output: TaskPlanOutput = self.agent.run(task_data)
+        
+        results = []
+        
+        state = CodingState(
+            prompt=None,
+            messages=[],
+            inputs={},
+            candidate=None,
+            node=None,
+            llm=self.llm_factory.create_chat_model(LLMType.DEFAULT)
+        )
+
+        for task in output.tasks:
+            tool = task.tool_id
+            
+            state["prompt"] = task.prompt
+
+            # Get the class reference dynamically
+            cls = globals().get(tool, None)
+
+            if cls is not None:
+                # Create an instance of the class
+                instance = cls()
+
+                # Call the _run method of the instance
+                state = instance._run(state)
+                print(state)
+                results.append(state)
+            else:
+                print(f"Tool '{tool}' not found.")
+        
+        # Arrange
+
+        
+        retrieval = retrieve_context()
+
+        # Act
+        retrival_state = retrieval._run(state)
+        
+        docs = [doc.metadata['source'] for doc in retrival_state["context"]]
+        
+        state["context"] = docs
+        
+        aider = use_aider()
+        
+        aider_state = aider._run(retrival_state)
+        
+        
+        draft = draft_code()
+        
+        state = draft._run(state)
+        
+        print(state)
+
+        
+        
+
+
+    def test_run_integration(self):
+        problem_description = "Given a list of integers, find the sum of all even numbers in the list."
+        test_cases = [
+            {"input": "1 2 3 4 5", "output": "6"},
+            {"input": "6 7 8 9 10", "output": "18"}
+        ]
+        runtime_limit = 5  # seconds
+
+
+
+ 
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ agents/coding_agent/tools/core.py
@@ -0,0 +1,103 @@
+from langchain_core.tools.base import BaseTool
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.tools import tool
+from langchain_community.retrievers import BM25Retriever
+from langchain_community.document_loaders import PythonLoader
+from langchain_chroma import Chroma
+import os
+from langchain.document_loaders import PythonLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters.python import PythonCodeTextSplitter
+from langchain_aws.embeddings.bedrock import BedrockEmbeddings
+from langchain_community.retrievers import BM25Retriever
+
+from agents.coding_agent.types import CodingState, WriteCode
+
+
+from aider.coders import Coder
+from aider.models import Model
+from aider.io import InputOutput
+
+class use_aider(BaseTool):
+    name: str = "use_aider"
+    description: str = "Use this tool when needing to make code edits. Input should be a problem/prompt description, and a list of files to edit, and an optional read only list for context"
+    def _run(self, state: CodingState) -> CodingState:
+        io = InputOutput(yes=True)
+        model = Model("bedrock/us.anthropic.claude-3-sonnet-20240229-v1:0")
+        
+        coder = Coder.create(main_model=model, fnames=state["context"], io=io)
+        
+        coder.run(state["prompt"])
+        
+        return state
+        
+
+
+class draft_code(BaseTool):
+    name: str = "draft_code"
+    description: str = "Useful for when you need to draft code. Input should be a problem description and code to modify. Output should be a draft code solution in the language defined in the prompt."
+    
+    def _run(self, state: CodingState) -> CodingState:
+        draft_parser = PydanticOutputParser(pydantic_object=WriteCode)
+
+        draft_prompt = PromptTemplate(
+            input_variables=["prompt", "context", "language"],
+            template="Relevant Context:\n```\n{context}\n```\n\n"
+                    "Given the following programming problem description and code to modify, provide a draft {language} code solution. Refernce the code above when necessary.\n\n"
+                    "Prompt: {prompt}\n\n"
+                    "Your output should follow the provided JSON schema:\n\n{format_instructions}\n\n"
+                    "Only respond with the JSON and no additional formatting or comments, do not include anything else besides the json output\n"
+                    "Draft Solution:\n",
+            partial_variables={"format_instructions": draft_parser.get_format_instructions()},
+        )
+
+        draft_chain = draft_prompt | state["llm"] | draft_parser
+
+        draft = draft_chain.invoke({"prompt": state["prompt"], "context": state["context"], "language": state["language"]})
+        
+        state["candidate"] = draft
+        state["messages"].append(draft)
+        state["status"] = "draft"
+        
+        return state
+        
+        
+class retrieve_context(BaseTool):
+    name: str = "retrieve_context"
+    description: str = "Useful for when you need to retrieve relevant context. Input should be a problem description, code to modify and a corpus of docs such as a code repo. Output should be the relevant context from the corpus."
+    
+    def _run(self, state: CodingState) -> CodingState:
+        docs = self.load_files("/root/personal/generative-agent-test2/supervisor")
+        
+        text_splitter = PythonCodeTextSplitter(chunk_size=2000, chunk_overlap=500)
+        splits = text_splitter.split_documents(docs)
+        vectorstore = Chroma.from_documents(documents=splits, embedding=BedrockEmbeddings())
+        
+        vector_retriever = vectorstore.as_retriever()
+        vector_results = vector_retriever.get_relevant_documents(state["prompt"])
+        
+        #bm25_retriever = BM25Retriever.from_documents(splits)
+        #semantic_results = bm25_retriever.get_relevant_documents(state["prompt"])
+        
+        docs = [doc.metadata['source'] for doc in vector_results]
+        
+        state["context"] = docs
+               
+        return state
+
+    def load_files(self, base_dir):
+        documents = []
+
+        for root, dirs, files in os.walk(base_dir):
+            for file in files:
+                if file.endswith('.py'):
+                    file_path = os.path.join(root, file)
+                    python_loader = PythonLoader(file_path)
+                    documents.extend(python_loader.load())
+                elif file.endswith('.md'):
+                    file_path = os.path.join(root, file)
+                    markdown_loader = TextLoader(file_path)
+                    documents.extend(markdown_loader.load())
+
+        return documents
